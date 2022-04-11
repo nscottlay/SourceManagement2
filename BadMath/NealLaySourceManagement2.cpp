d23eaa9aa618a4ac32c9b17570a5e195d8fa18d0
Neal Lay <neal.lay@sinclair.edu>
2022-04-11 14:15:31 -0400

    Source from BadMath with fixed bugs.

--- BadMath/NealLaySourceManagement2.cpp
+++ BadMath/NealLaySourceManagement2.cpp
@@ -1,23 +1,33 @@
+// Neal Lay
+// CIS 1202 501
+// April 11,2022
+
 #include <iostream>
 #include <iomanip>
+
 using namespace std;
 
-float average(int i1, int i2, int i3)
-{
-	return i1 + i2 + i3 / 3.0f;
-}
+float average(int, int, int);
 
 int main()
 {
-	int n1 =0 , n2 = 0, n3 = 0;
+	int n1 = 0 , n2 = 0, n3 = 0;
+
 	cout << "This program calculates the average of three numbers." << endl;
 	cout << "First number: "; cin >> n1;
 	cout << "Second number: "; cin >> n2;
-	cout << "Third number: "; cin >> n2;
+	cout << "Third number: "; cin >> n3; // Changed n2 to n3
 
 	float a = average(n1, n2, n3);
 
 	cout << setprecision(1) << fixed << "The average is " << a << endl;
 
 	return 0;
+}
+
+// Moved function below main function
+float average(int i1, int i2, int i3)
+{
+	// Put parenthesis around i variables for addition
+	return (i1 + i2 + i3) / 3.0f;
 }
